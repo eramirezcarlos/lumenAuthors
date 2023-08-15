74bdaefb427d3519da94c61c0934da564e808e31
data_analyst <tequendama.developer@gmail.com>
2023-08-15 01:03:22 -0400

    Methods consistency

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ $router->get('/', function () use ($router) {
 
 $router->get('/authors', 'AuthorController@index');
 $router->post('/authors', 'AuthorController@store');
-$router->post('/authors/{author}', 'AuthorController@show');
+$router->get('/authors/{author}', 'AuthorController@show');
 $router->put('/authors/{author}', 'AuthorController@update');
 $router->patch('/authors/{author}', 'AuthorController@update');
 $router->delete('/authors/{author}', 'AuthorController@destroy');
